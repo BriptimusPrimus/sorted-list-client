351b4aeb61c092e9dc3b68a727cb7d0a1503b0dc
briptimus <brituor@gmail.com>
2016-05-03 19:50:06 -0700

    Adding bootstrap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
 console.log('Aloha World!');
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,33 @@ module.exports = {
     './src/index.js'
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        loader:"url?prefix=font/&limit=5000"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
