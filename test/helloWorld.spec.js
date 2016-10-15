f21913696f7146286305eadacee32009cfae4d0a
briptimus <brituor@gmail.com>
2016-10-15 10:33:15 -0700

    Seting up test suit

---
+++ test/helloWorld.spec.js
@@ -0,0 +1,9 @@
+// Tests setup is inspired in these tutorials:
+// http://spencerdixon.com/blog/test-driven-react-tutorial.html
+// https://semaphoreci.com/community/tutorials/testing-react-components-with-enzyme-and-mocha
+
+describe('hello world', () => {
+  it('works!', () => {
+    expect(true).to.be.true;
+  });
+});
