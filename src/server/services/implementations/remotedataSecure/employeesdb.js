4754354a2308ccade620ef98302da1694657b574
Roberto Brito <brituor@gmail.com>
2020-12-08 14:01:31 -0800

    Secure DAL implementation

---
+++ src/server/services/implementations/remotedataSecure/employeesdb.js
@@ -0,0 +1,74 @@
+/*
+ * Data Access Service Implementation
+ */
+const conf = require('../../../../../config').dal.databases.employees;
+const { makeQuery } = require('./dbutils')(conf);
+
+const mapFields = function mapFields() {
+  return {
+    id: 'idpersona',
+    firstName: 'nombre',
+    surname: 'apaterno',
+    surname2: 'amaterno',
+    codeNumber: 'codigo',
+    rfc: 'rfc',
+    status: 'status'
+  };
+};
+
+const fetchEmployeesFromSource = async function fetchEmployeesFromSource({
+  count,
+  sortBy,
+  order = 'ASC'
+}) {
+  const orderBy = mapFields()[sortBy] || sortBy || 'codigo';
+  const direction = order === 'DESC' ? 'DESC' : 'ASC';
+  const sqlQry = `
+    SELECT
+        p.idpersona,
+        p.idmunicipio,
+        p.nombre,
+        p.apaterno,
+        p.amaterno,
+        p.codigo,
+        p.rfc,
+        p.homoclave,
+        p.nss,
+        p.status
+    FROM persona p
+    ORDER BY ?? ${direction} LIMIT ?;`;
+
+  const employees = await makeQuery({
+    sql: sqlQry,
+    values: [orderBy, count]
+  });
+  return employees.map(emp => ({
+    id: emp.idpersona,
+    firstName: emp.nombre,
+    surname: emp.apaterno,
+    surname2: emp.amaterno,
+    codeNumber: emp.codigo,
+    rfc: emp.rfc,
+    status:
+      emp.status.toLowerCase() === 'contratado'
+        ? 'hired'
+        : emp.status.toLowerCase()
+  }));
+};
+
+const getEmployees = async function getEmployees(opts) {
+  try {
+    const employees = await fetchEmployeesFromSource(opts);
+    return employees;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+// This object implements the bridge interface:
+// interface: {
+//   getEmployees: async function(options){}
+// }
+module.exports = {
+  getEmployees
+};
--- src/server/services/implementations/remotedataSecure/index.js
+++ src/server/services/implementations/remotedataSecure/index.js
@@ -1,9 +1,14 @@
-const getEmployees = function getEmployees() {};
+const { getEmployees } = require('./employeesdb');
+const { getCustomer, getCustomers } = require('./customersdb');
 
 // This object implements the bridge interface:
 // interface: {
+//   getCustomer: async function(customerId){}
+//   getCustomers: async function(options){}
 //   getEmployees: async function(options){}
 // }
 module.exports = {
+  getCustomer,
+  getCustomers,
   getEmployees
 };
